c8cb0e51d35bcc02ce253a64e5306243f3e53459
kdbanman <kdbanman@ualberta.ca>
2014-07-05 02:42:57 -0600

    improve format of separator

--- post-push.sh
+++ post-push.sh
@@ -1,9 +1,15 @@
 id=$(openssl rand -base64 8)
 
+echo "============" >> out.log
+echo "" >> out.log
 echo $id >> out.log
+echo "" >> out.log
 echo "============" >> out.log
 
+echo "============" >> err.log
+echo "" >> err.log
 echo $id >> err.log
+echo "" >> err.log
 echo "============" >> err.log
 
 git pull && npm install && ( forever restart -a -l forever.log -o out.log -e err.log srv.js || forever start -a -l forever.log -o out.log -e err.log srv.js )
