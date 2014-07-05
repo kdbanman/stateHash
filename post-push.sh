ecc171e960d3647d9cfc2a9bc3e94cefbb26ed0f
kdbanman <kdbanman@ualberta.ca>
2014-07-05 02:33:40 -0600

    remove bad newline

--- post-push.sh
+++ post-push.sh
@@ -1,9 +1,9 @@
 id=$(openssl rand -base64 8)
 
 echo $id >> out.log
-echo "============\n" >> out.log
+echo "============" >> out.log
 
 echo $id >> err.log
-echo "============\n" >> err.log
+echo "============" >> err.log
 
 git pull && npm install && ( forever restart -a -l forever.log -o out.log -e err.log srv.js || forever start -a -l forever.log -o out.log -e err.log srv.js )
